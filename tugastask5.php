6f3db37119619b35502ebf2f5dbd997ae6dc94a9
Biyan <nuryantorobi736@gmail.com>
2024-10-30 20:38:41 +0700

    Add files via upload

---
+++ tugastask5.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"
+    harset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Registrasi</title>
+    <style>
+        /* Gaya CSS sama dengan sebelumnya */
+    </style>
+</head>
+<body>
+<table class="table table-dark">
+  <thead>
+    
+  </thead>
+  <tbody>
+    <tr class="table-active">
+    <tr>
+      <td>Nama</td>
+      <td>Umur</td>
+      <td>Gender</td>
+      <td>Pendidikan</td>
+      <td>Hobi</td>
+
+    </tr>
+    <tr>
+      <td>Bedu</td>
+      <td>17 tahun</td>
+      <td>Laki-Laki</td>
+      <td>S1</td>
+      <td>Membaca Buku|Travelling|Olahraga</td>
+      
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
